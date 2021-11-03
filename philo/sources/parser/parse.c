c9c1e194ef164d58ac6615565c7fff6e8dbc8fce
LoisDuplain <loisdupl42@gmail.com>
2021-11-03 11:20:41 +0100

    Backup Wednesday, November 03, 2021 11:20

--- philo/sources/parser/parse.c
+++ philo/sources/parser/parse.c
@@ -6,7 +6,7 @@
 /*   By: lduplain <lduplain@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 15:33:14 by lduplain          #+#    #+#             */
-/*   Updated: 2021/11/03 09:29:19 by lduplain         ###   ########.fr       */
+/*   Updated: 2021/11/03 11:19:20 by lduplain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,12 @@ int	parse(t_simulation *sim, int argc, char **argv)
 	sim->time_to_sleep = ft_atoi(argv[3]);
 	if (sim->n_philosophers <= 0)
 	{
-		printf("Number of philosophers must be greater than 0.\n");
+		printf("Wrong number of philosophers.\n");
 		return (0);
 	}
 	if (sim->time_to_die < 0 || sim->time_to_eat < 0 || sim->time_to_sleep < 0)
 	{
-		printf("Error in your time values.\n");
+		printf("Wrong time value.\n");
 		return (0);
 	}
 	sim->n_times_each_philosopher_must_eat = 0;
@@ -34,7 +34,7 @@ int	parse(t_simulation *sim, int argc, char **argv)
 		sim->n_times_each_philosopher_must_eat = ft_atoi(argv[4]);
 		if (sim->n_times_each_philosopher_must_eat <= 0)
 		{
-			printf("n_times_each_philosopher_must_eat must be > 0.\n");
+			printf("Wrong number of times each philosopher must eat.\n");
 			return (0);
 		}
 	}
